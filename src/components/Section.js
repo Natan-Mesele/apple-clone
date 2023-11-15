0097dc72e8848010640ce6444692edc56521d60e
Natan-Mesele <huluale12@gmail.com>
2023-11-15 22:23:11 +0300

    home

---
+++ src/components/Section.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Section() {
+  return (
+    <Container>
+        <Item>
+
+        </Item>
+    </Container>
+  )
+}
+
+export default Section
+
+const Container = styled.div`
+    background: url("img/watch-ser.jpg");
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+    width: 100vw;
+    height: 100vh;
+
+`
+const Item = styled.div`
+
+`
